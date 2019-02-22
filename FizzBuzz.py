de195579c0d55bbb543ae273a3f2bdc190b6c386
pythongirl1987 <47753954+pythongirl1987@users.noreply.github.com>
2019-02-22 20:50:27 +0100

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+number = int(input("Select a number between 1 and 100: "))
+
+for x in range(number + 1):
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 5 == 0:
+        print("buzz")
+    elif x % 3 == 0:
+        print("fizz")
+    else:
+        print(x)
+
+
+
