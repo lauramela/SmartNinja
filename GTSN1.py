0234432fe3402effa3c9fc4efd84aae464ae0e7b
lauramela <47753954+lauramela@users.noreply.github.com>
2019-03-09 11:11:17 +0100

    Delete GTSN1.py

--- GTSN1.py
+++
@@ -1,7 +0,0 @@
-secret = "10"
-guess = input("What is the secret number? ")
-
-if guess == secret:
-    print(f"Congratulations, the number {guess} is correct!")
-else:
-    print("Oeps, please try again!")
